afa964deccbbb14d31b4c58fe33ebf2d69e751d4
kunigami <kunigami@gmail.com>
2017-03-13 10:36:13 -0700

    adding the push function + tests

--- queue/queue_suspended.ml
+++ queue/queue_suspended.ml
@@ -5,22 +5,71 @@
     example, using the Physicist's method as described in Okazaki's Purely
     Functional Data Structures, Chapter 6.
 
-    Only the front list needs to be lazy. The back of the queue is a regular
+    Only the front list needs to be lazy. The rear of the queue is a regular
     list. We store a copy of the suspended list to be able to access the head of
     the list without having to evaluate the entire list.
 
-    The first element in the structure is the evaluated version of the front.
-    The second represents the size of the front list. The third element is the
-    suspended version of the front. The fourth element is the size of the back
-    list and finally the fifth element is the back list.
+    The first element in the structure is the evaluated version of the front
+    (forcedFront). The second represents the size of the front list (frontSize).
+    The third element is the suspended version of the front (lazyFront). The
+    fourth element is the size of the rear list (rearSize) and finally the fifth
+    element is the rear list (rear).
 
-    The invariant that must be respected by all operations is that the front
-    list must never be smaller than the back list.
+    The invariants that must be respected by all operations are:
+    1) The front list must never be smaller than the rear list
+    2) Whenever lazyFront is non-empty, forcedFront is non-empty
 *)
 type 'a queueSuspended = 'a list * int * ('a list) Lazy.t * int * 'a list;;
 
 let newEmpty = ([], 0, lazy [], 0, [])
 
 let isEmpty (queue: 'a queueSuspended): bool = match queue with
-  (_, leftSize, _, _, _) -> leftSize == 0
+  (_, frontSize, _, _, _) -> frontSize == 0
 ;;
+
+(* let push (elem: 'a) (queue: 'a queueSuspended): ('a queueSuspended) = match queue with
+  (leftSize, left, rightSize, right) ->
+    check (leftSize, left, rightSize + 1, Stream2.insert elem right)
+;; *)
+
+(*
+  Makes sure the queue satisfies the invariant: If lazyFront is non-empty,
+  forcedFront is non-empty
+*)
+let checkForcedFrontInvariant (queue: 'a queueSuspended): ('a queueSuspended) =
+  match queue with
+    | ([], frontSize, lazyFront, rearSize, rear) ->
+      (Lazy.force lazyFront, frontSize, lazyFront, rearSize, rear)
+    | queue -> queue
+;;
+
+(*
+  Makes sure the queue satisfies the invariant: The front list must never be
+  smaller than the rear list
+*)
+let checkInvariants (
+  queue: 'a queueSuspended
+): ('a queueSuspended) = match queue with
+  (forcedFront, frontSize, lazyFront, rearSize, rear) ->
+    if rearSize <= frontSize then checkForcedFrontInvariant queue
+    (* Invariant violated: reverse rear and concatenate w/ frontLazy. Not that
+    we do not update frontForced with the newest version of front *)
+    else let front = Lazy.force lazyFront
+      in checkForcedFrontInvariant (
+        front,
+        frontSize + rearSize,
+        lazy (front @ (List.rev rear)),
+        0,
+        []
+      )
+;;
+
+let push (queue: 'a queueSuspended) (elem: 'a): ('a queueSuspended) =
+  match queue with (forcedFront, frontSize, lazyFront, rearSize, rear) ->
+    checkInvariants (
+      forcedFront,
+      frontSize,
+      lazyFront,
+      rearSize + 1,
+      elem :: rear
+    )
--- queue/queue_suspended_test.ml
+++ queue/queue_suspended_test.ml
@@ -6,10 +6,36 @@ let testEmptyCheck text_ctx =
   (Queue_suspended.isEmpty Queue_suspended.newEmpty)
 ;;
 
+let assertInvariants (queue: 'a queueSuspended) =
+  match queue with (forcedFront, frontSize, lazyFront, rearSize, rear) ->
+    let front = Lazy.force lazyFront
+    in
+      assert_bool
+        "Length of front should be reflected in frontSize"
+        (
+          ((List.length front) == frontSize) &&
+          ((List.length rear) == rearSize)
+        )        
+;;
+
+let queueToList (queue: 'a queueSuspended): 'a list =
+  match queue with (forcedFront, frontSize, lazyFront, rearSize, rear) ->
+    let front = Lazy.force lazyFront
+    in front @ (List.rev rear)
+
+let testSingleInsertion text_ctx =
+  let result = (Queue_suspended.push (Queue_suspended.newEmpty) 10)
+  in let () = assertInvariants result
+  in assert_equal
+    ~msg:"Should insert one element properly"
+    (queueToList result)
+    [10]
+
 let suite =
 "suite">:::
  [
   "testEmptyCheck">:: testEmptyCheck;
+  "testSingleInsertion">:: testSingleInsertion;
  ]
 ;;
 
