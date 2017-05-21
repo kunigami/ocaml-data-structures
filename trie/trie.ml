6e685b83ce11417be794c7cf3521f9345e495d6b
kunigami <kunigami@gmail.com>
2017-05-20 23:06:53 -0700

    adding more methods to the trie structure: size, words count, merge and equals

--- trie/trie.ml
+++ trie/trie.ml
@@ -1,4 +1,4 @@
-open Batteries;;
+open BatString;;
 
 exception Empty_string;;
 
@@ -12,8 +12,36 @@ type trie = Node of trie ChildList.t * bool;;
 
 let empty: trie = Node (ChildList.empty, false);;
 
+let rec sizeImpl (trie: trie): int =
+  let Node(children, _) = trie in
+    ChildList.fold
+      (fun char_key node accSize -> accSize + (sizeImpl node))
+      children
+      (* accounts for the current node *)
+      1
+;;
+
+(* Returns the number of nodes in a trie *)
+let size (trie: trie): int =
+  (* We need to discount one because the root node does not represent a character *)
+  (sizeImpl trie) - 1
+;;
+
+(*
+   Returns the number of words in a trie, or the number of nodes
+   withhasEntry=true
+*)
+let rec entriesCount (trie: trie): int =
+  let Node(children, hasEntry) = trie in
+    ChildList.fold
+      (fun char_key node accSize -> accSize + (entriesCount node))
+      children
+      (* accounts for the current node *)
+      (if hasEntry then 1 else 0)
+;;
+
 let rec insert (s: char list) (trie: trie) : trie =
-  let Node(children, isLeaf) = trie in match s with
+  let Node(children, hasEntry) = trie in match s with
     | [] -> Node(children, true)
     | first_char :: rest ->
       let currentChild = if ChildList.mem first_char children
@@ -24,20 +52,52 @@ let rec insert (s: char list) (trie: trie) : trie =
       let newChildren = ChildList.add first_char newChild children in
       Node(
         newChildren,
-        isLeaf
+        hasEntry
       )
+;;
+
+let rec merge (trieA: trie) (trieB: trie): trie =
+  let Node(childrenA, hasEntryA) = trieA in
+  let Node(childrenB, hasEntryB) = trieB in
+  let mergedChildren = ChildList.union
+    (fun key nextNodeA nextNodeB -> Some (merge nextNodeA nextNodeB))
+    childrenA
+    childrenB
+  in Node(mergedChildren, hasEntryA || hasEntryB)
+;;
 
 (* Convenience method to insert a string instead of a list of characters *)
 let insertString (s: string) (trie: trie): trie =
-  insert (String.to_list s) trie;;
+  insert (BatString.to_list s) trie;;
+;;
 
 let rec has (s: char list) (trie: trie): bool =
-  let Node(children, isLeaf) = trie in match s with
-    | [] -> isLeaf
+  let Node(children, hasEntry) = trie in match s with
+    | [] -> hasEntry
     | first_char :: rest ->
       if ChildList.mem first_char children then
         has rest (ChildList.find first_char children)
       else false
+;;
+
+let rec equals (trieA: trie) (trieB: trie): bool =
+  let Node(childrenA, hasEntryA) = trieA in
+  let Node(childrenB, hasEntryB) = trieB in
+  if hasEntryA != hasEntryB then false
+  else
+    (
+      ChildList.for_all (fun chr nextChildA ->
+        ChildList.mem chr childrenB &&
+        equals nextChildA (ChildList.find chr childrenB)
+      ) childrenA
+    ) &&
+    (
+      ChildList.for_all (fun chr nextChildB ->
+        ChildList.mem chr childrenA &&
+        equals nextChildB (ChildList.find chr childrenA)
+      ) childrenB
+    )
+;;
 
 (* Convenience method to search for a string instead of a list of characters *)
-let hasString (s: string) (trie: trie): bool = has (String.to_list s) trie;;
+let hasString (s: string) (trie: trie): bool = has (BatString.to_list s) trie;;
--- trie/trie_test.ml
+++ trie/trie_test.ml
@@ -11,6 +11,11 @@ let testQueryOnEmptyTrie text_ctx =
   assert_bool "Should not contain any string" (not (Trie.hasString "a" emptyTrie))
 ;;
 
+let testComparingTries text_ctx =
+  let trieA = Trie.(empty |> insertString "abc" |> insertString "bc") in
+  let trieB = Trie.(empty |> insertString "bc" |> insertString "abc") in
+  assert_bool "Insertion order does not matter" (Trie.equals trieA trieB)
+
 let testSimpleInsertionAndQuery text_ctx =
   let sampleTrie = Trie.(empty |> insertString "abc" |> insertString "abd") in
   let () = assert_bool "Should contain string abc" (Trie.hasString "abc" sampleTrie) in
@@ -53,15 +58,57 @@ let testSameLetter text_ctx =
   ()
 ;;
 
+let testSize text_ctx =
+  let sampleTrie = Trie.(empty |> insertString "abc" |> insertString "ab" |> insertString "a") in
+  assert_equal ~msg:"It should contain 3 characters" (Trie.size sampleTrie) 3
+
+let testEntriesCount text_ctx =
+  let sampleTrie = Trie.(
+    empty |>
+    insertString "abc" |>
+    insertString "ab" |>
+    insertString "a" |>
+    (* Trie should  include repeated entries only once *)
+    insertString "a" |>
+    insertString "bc" |>
+    insertString "bc"
+  ) in
+  assert_equal
+    ~msg:"It should contain 4 unique entries"
+    (Trie.entriesCount sampleTrie)
+    4
+;;
+
+let testMergingTries text_ctx =
+  let trieA = Trie.(empty |> insertString "abc" |> insertString "ab" |> insertString "c") in
+  let trieB = Trie.(empty |> insertString "ab" |> insertString "cd" |> insertString "d") in
+  let mergedTrie = Trie.merge trieA trieB in
+  let expectedTrie = Trie.(
+    empty |>
+    insertString "ab" |>
+    insertString "abc" |>
+    insertString "c" |>
+    insertString "cd" |>
+    insertString "d"
+  ) in
+  assert_bool
+    "Merging two tries is equivalent to a trie with the words from both tries"
+    (Trie.equals mergedTrie expectedTrie)
+;;
+
 let suite =
 "suite">:::
  [
   "testQueryOnEmptyTrie">:: testQueryOnEmptyTrie;
+  "testComparingTries">:: testComparingTries;
   "testSimpleInsertionAndQuery">:: testSimpleInsertionAndQuery;
   "testPrefixes">:: testPrefixes;
   "testPrefixesReversed">:: testPrefixesReversed;
   "testPrefixes2">:: testPrefixes2;
   "testSameLetter">:: testSameLetter;
+  "testSize">:: testSize;
+  "testEntriesCount">:: testEntriesCount;
+  "testMergingTries">:: testMergingTries;
  ]
 ;;
 
